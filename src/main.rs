0dd0d07106f1051052ee5d64712a7392613148ab
erikloma27 <erikloma27@gmail.com>
2026-01-10 20:52:17 +0100

    Created main menu function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
+fn main_menu() -> String {
+    println!("To-do Application");
+    println!("Insert an option:");
+    println!("1. Show pending tasks");
+    println!("2. Add a new task");
+    println!("3. Remove a task");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to option");
+
+    guess
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = main_menu();
+
+    println!("{opt}")
 }
